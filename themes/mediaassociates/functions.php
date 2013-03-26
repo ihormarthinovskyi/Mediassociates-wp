6c3fedd7e0f04e845470c6521f02e1ebe18c910c
Bryan Bielefeldt <bbielefeldt@thepowertoprovoke.com>
2013-03-26 08:46:26 -0400

    created dynamic sidebar for interior pages
    functions.php and sidebar.php
    if widgets dont exist the default (hardcoded) will display

--- themes/mediaassociates/functions.php
+++ themes/mediaassociates/functions.php
@@ -42,4 +42,67 @@
 	remove_action('wp_head', 'start_post_rel_link', 10, 0); 
 	remove_action('wp_head', 'parent_post_rel_link', 10, 0); 
 	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0); 
+	
+	/**
+	 *
+	 * do shrotcode in sidebar
+	 *
+	 */
+	add_filter('widget_text', 'do_shortcode');
+	
+	/**
+	 *
+	 * span in widget titles
+	 * use #span text #/span
+	 */
+	function kc_convert_title_html_tags( $string ) {
+	  global $wp_current_filter;
+	  $filter = end($wp_current_filter);
+	  $search = array('[span] ', ' [/span]');
+	  $replace = ( in_array($filter, array('wp_title', 'the_title_rss')) || ($filter == 'the_title' && (is_admin() || in_array('wp_head', $wp_current_filter))) ) ? '' : array('<span>', '</span>');
+	  $string = str_replace( $search, $replace, $string );
+	
+	  return $string;
+	} add_filter( 'widget_title', 'kc_convert_title_html_tags' );
+	
+	/**
+	 * Register our sidebars and widgetized areas.
+	 *
+	 */
+	function MA_init() {
+	 	// right top interior
+		register_sidebar( array(
+			'name' => 'Interior Right Top',
+			'id' => 'interior_right_1',
+			'before_widget' => '<div class="form-holder">',
+			'after_widget' => '</div><!--form-holder-->',
+			'before_title' => '<h3>',
+			'after_title' => '</h3>',
+		) );
+		// right bottom interior
+		register_sidebar( array(
+			'name' => 'Interior Right bottom',
+			'id' => 'interior_right_2',
+			'before_widget' => '<div class="rss-box">',
+			'after_widget' => '</div><!--rss-box-->',
+			'before_title' => '<h3>',
+			'after_title' => '</h3>',
+		) );
+	}
+	add_action( 'widgets_init', 'MA_init' );
+	
+	// [sidebar_form] shorcode
+	function sidebar_form_func( $atts ) {
+		extract( shortcode_atts( array(
+			'form' => 'form',
+			'type' => 'gravity',
+		), $atts ) );
+		ob_start();
+		echo get_template_part($form,$type);
+		$output_form = ob_get_contents();
+		ob_end_clean();
+		return $output_form;
+	}
+	add_shortcode( 'sidebar_form', 'sidebar_form_func' );
+	
 ?>
--- themes/mediaassociates/sidebar.php
+++ themes/mediaassociates/sidebar.php
@@ -1,13 +1,24 @@
 <aside>
-	<div class="form-holder">
-		<h3>How MAY we help <span>you</span>?</h3>
-		<?php get_template_part('form','gravity'); ?>
-
-	</div><!--form-holder-->
-	<div class="rss-box">
-		<a href="#" class="rss">rss</a>
-		<h3><span>BETTER</span> THINKING</h3>
-		<p>Read our thoughts about emerging opportunities in media.</p>
-		<a href="#" class="download">DOWNLOAD WHITEPAPERS</a>
-	</div><!--rss-box-->
+	<?php 
+	if ( dynamic_sidebar('interior_right_1') ) : 
+	else : 
+	?>
+		<div class="form-holder">
+			<h3>How MAY we help <span>you</span>?</h3>
+			<?php get_template_part('form','gravity'); ?>
+	
+		</div><!--form-holder-->
+	<?php endif; ?>
+	<?php 
+	if ( dynamic_sidebar('interior_right_2') ) : 
+	else : 
+	?>
+		<div class="rss-box">
+			<h3><span>BETTER</span> THINKING</h3>
+			<a href="#" class="rss">rss</a>
+			<p>Read our thoughts about emerging opportunities in media.</p>
+			<a href="#" class="download">DOWNLOAD WHITEPAPERS</a>
+		</div><!--rss-box-->
+	<?php endif; ?>
+	
 </aside>
