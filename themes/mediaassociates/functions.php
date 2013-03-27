236053f82ac40f1b73dbe1c7c04cef7a6724098d
Bryan Bielefeldt <bbielefeldt@thepowertoprovoke.com>
2013-03-27 09:09:41 -0400

    theme functions

--- themes/mediaassociates/functions.php
+++ themes/mediaassociates/functions.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 	add_theme_support( 'menus' );
 	add_theme_support( 'post-thumbnails' );
 	add_image_size( 'inner_hero', 9999, 162 );
@@ -42,6 +43,53 @@
 	remove_action('wp_head', 'feed_links_extra', 3); 
 	remove_action('wp_head', 'start_post_rel_link', 10, 0); 
 	remove_action('wp_head', 'parent_post_rel_link', 10, 0); 
+=======
+
+	add_theme_support( 'menus' );
+	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'inner_hero', 9999, 162 );
+	add_image_size( 'blog_image', 607, 241 );
+	add_image_size( 'leader-thumb', 95, 95, true );
+	add_image_size( 'case-image', 539, 300, true );
+	add_image_size( 'case-thumb', 79, 48, true );
+
+	add_action( 'init', 'register_my_menu' );
+	function register_my_menu() {
+		register_nav_menu( 'main_menu', __( 'Main Menu' ) );
+
+		register_nav_menu( 'services_menu', __( 'Services Menu' ) );
+		register_nav_menu( 'mobile_menu', __( 'Mobile Menu' ) );
+
+	}
+
+	function ss_init_script() {
+		if (!is_admin()) {
+
+		   wp_deregister_script( 'jquery' );
+		   wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js');
+		   wp_enqueue_script( 'jquery','1.8.2');
+		   wp_enqueue_script('ui', get_bloginfo('template_directory').'/js/jquery-ui-1.9.2.custom.min.js', array('jquery'), '1.0');
+		   wp_enqueue_script('flex', get_bloginfo('template_directory').'/js/jquery.flexslider-min.js', array('jquery'), '1.0');
+		   wp_enqueue_script('customform', get_bloginfo('template_directory').'/js/custom-form.js', array('jquery'), '1.0');
+		   wp_enqueue_script('customselect', get_bloginfo('template_directory').'/js/custom-form.select.js', array('jquery'), '1.0');
+		   wp_enqueue_script('inputs', get_bloginfo('template_directory').'/js/clear-inputs.js', array('jquery'), '1.0');
+		   wp_enqueue_script('slideblock', get_bloginfo('template_directory').'/js/slideBlock.js', array('jquery'), '1.0');
+		   wp_enqueue_script('script', get_bloginfo('template_directory').'/js/scripts.js', array('jquery'), '1.0');
+		}
+	}
+	add_action('wp_enqueue_scripts', 'ss_init_script'); 
+
+
+	remove_action('wp_head', 'rel_canonical'); 
+	remove_action('wp_head', 'rsd_link'); 
+	remove_action('wp_head', 'wp_generator'); 
+	remove_action('wp_head', 'feed_links', 2); 
+	remove_action('wp_head', 'index_rel_link'); 
+	remove_action('wp_head', 'wlwmanifest_link'); 
+	remove_action('wp_head', 'feed_links_extra', 3); 
+	remove_action('wp_head', 'start_post_rel_link', 10, 0); 
+	remove_action('wp_head', 'parent_post_rel_link', 10, 0); 
+>>>>>>> theme functions
 	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0); 
 	
 	/**
--- themes/mediaassociates/page-clients.php
+++ themes/mediaassociates/page-clients.php
@@ -9,238 +9,46 @@ get_header(); ?>
 		<?php get_template_part('header','inner'); ?>
 		<section id="main">
 			<section id="content">
-				<h2 id="clients">CLIENTS</h2>
-				<p class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
-				<div class="clients-holder">
-					<div class="col">
-						<strong class="ttl">Financial</strong>
-						<ul class="list">
-							<li>Alliance Credit Union</li>
-							<li>Keesler Credit Union</li>
-							<li>Fairfield County Bank</li>
-							<li>National College Finance Center</li>
-							<li>Navy Federal Credit Union</li>
-							<li>North American Savings Bank</li>
-							<li>Opus Bank</li>
-							<li>PURE</li>
-							<li>Validus</li>
-						</ul>
-						<strong class="ttl">Education</strong>
-						<ul class="list">
-							<li>Balance Diet</li>
-							<li>Bellevue University</li>
-							<li>Bryant University</li>
-							<li>Cambridge University</li>
-							<li>Franklin Pierce University</li>
-							<li>Fraser Woods School</li>
-							<li>Ithaca College</li>
-							<li>Huntington Learning Centers</li>
-							<li>Hyde Schools</li>
-							<li>Manhattanville College</li>
-							<li>Marist College</li>
-							<li>Opus Bank</li>
-							<li>Stonehill College</li>
-							<li>Southern New Hampshire University</li>
-							<li>University of Connecticut</li>
-							<li>University of Miami</li>
-						</ul>
-						<strong class="ttl">Health &amp; Government</strong>
-						<ul class="list">
-							<li>Academy for Educational Development</li>
-							<li>American Red Cross</li>
-							<li>Astra-Zeneca</li>
-							<li>Ausanil</li>
-							<li>Bioderm</li>
-							<li>Centers for Medical Weight Loss</li>
-							<li>Cognizant</li>
-							<li>Danbury Hospital</li>
-							<li>Genesis Healthcare</li>
-							<li>GlaxoSmithKline</li>
-							<li>Hartford Hospital</li>
-							<li>Fitness Edge</li>
-							<li>Men’s Liberty</li>
-							<li>New Milford Hospital</li>
-						</ul>
-					</div><!--col-->
-					<div class="col">
-						<ul class="list">
-							<li>Oak Ridge Associated Universities</li>
-							<li>Orthovisc</li>
-							<li>Prostacycle</li>
-							<li>State of Connecticut Department of Public Health</li>
-							<li>The Centers for Disease Control</li>
-							<li>U.S. Department of Health &amp; Human </li>
-							<li>Services</li>
-							<li>U.S. Department of Homeland Security</li>
-							<li>Varian Medical Systems</li>
-							<li>Western Connecticut Health Network </li>
-							<li>Foundation</li>
-							<li>Windham Hospital</li>
-						</ul>
-						<strong class="ttl">Technology &amp; Aviation</strong>
-						<ul class="list">
-							<li>3Com</li>
-							<li>ANSYS</li>
-							<li>Are You Interested?</li>
-							<li>Cessna Aircraft</li>
-							<li>Logitech</li>
-							<li>McAfee</li>
-							<li>Panasonic</li>
-							<li>VUDU</li>
-						</ul>
-						<strong class="ttl">Travel &amp; Entertainment</strong>
-						<ul class="list">
-							<li>Boyne Resorts</li>
-							<li>Fifth Degree Tours</li>
-							<li>Gaylord Hotels</li>
-							<li>Marriott</li>
-							<li>Newcastle Casino</li>
-							<li>Riverwind Casino</li>
-							<li>Remington Park</li>
-							<li>Silverleaf Resorts</li>
-							<li>Tauck</li>
-							<li>Westport Country Playhouse</li>
-							<li>WinStar World Casino</li>
-						</ul>
-						<strong class="ttl">Home, Energy &amp; Retail</strong>
-						<ul class="list">
-							<li>Astrum Solar</li>
-							<li>Atlas Energy</li>
-							<li>Black and Decker</li>
-							<li>Blu Homes</li>
-							<li>Canine Fence Company</li>
-							<li>Cumberland Farms</li>
-							<li>Guess Watches</li>
-							<li>Gym Source</li>
-						</ul>
-					</div><!--col-->
-				</div><!--clients-holder-->
-				<div class="accordion">
-					<strong class="ttl">Financial</strong>
-						<ul class="list">
-							<li>Alliance Credit Union</li>
-							<li>Keesler Credit Union</li>
-							<li>Fairfield County Bank</li>
-							<li>National College Finance Center</li>
-							<li>Navy Federal Credit Union</li>
-							<li>North American Savings Bank</li>
-							<li>Opus Bank</li>
-							<li>PURE</li>
-							<li>Validus</li>
-						</ul>
-						<strong class="ttl">Education</strong>
-						<ul class="list">
-							<li>Balance Diet</li>
-							<li>Bellevue University</li>
-							<li>Bryant University</li>
-							<li>Cambridge University</li>
-							<li>Franklin Pierce University</li>
-							<li>Fraser Woods School</li>
-							<li>Ithaca College</li>
-							<li>Huntington Learning Centers</li>
-							<li>Hyde Schools</li>
-							<li>Manhattanville College</li>
-							<li>Marist College</li>
-							<li>Opus Bank</li>
-							<li>Stonehill College</li>
-							<li>Southern New Hampshire University</li>
-							<li>University of Connecticut</li>
-							<li>University of Miami</li>
-						</ul>
-						<strong class="ttl">Health &amp; Government</strong>
-						<ul class="list">
-							<li>Academy for Educational Development</li>
-							<li>American Red Cross</li>
-							<li>Astra-Zeneca</li>
-							<li>Ausanil</li>
-							<li>Bioderm</li>
-							<li>Centers for Medical Weight Loss</li>
-							<li>Cognizant</li>
-							<li>Danbury Hospital</li>
-							<li>Genesis Healthcare</li>
-							<li>GlaxoSmithKline</li>
-							<li>Hartford Hospital</li>
-							<li>Fitness Edge</li>
-							<li>Men’s Liberty</li>
-							<li>New Milford Hospital</li>
-							<li>Oak Ridge Associated Universities</li>
-							<li>Orthovisc</li>
-							<li>Prostacycle</li>
-							<li>State of Connecticut Department of Public Health</li>
-							<li>The Centers for Disease Control</li>
-							<li>U.S. Department of Health &amp; Human </li>
-							<li>Services</li>
-							<li>U.S. Department of Homeland Security</li>
-							<li>Varian Medical Systems</li>
-							<li>Western Connecticut Health Network </li>
-							<li>Foundation</li>
-							<li>Windham Hospital</li>
-						</ul>
-						<strong class="ttl">Technology &amp; Aviation</strong>
-						<ul class="list">
-							<li>3Com</li>
-							<li>ANSYS</li>
-							<li>Are You Interested?</li>
-							<li>Cessna Aircraft</li>
-							<li>Logitech</li>
-							<li>McAfee</li>
-							<li>Panasonic</li>
-							<li>VUDU</li>
-						</ul>
-						<strong class="ttl">Travel &amp; Entertainment</strong>
-						<ul class="list">
-							<li>Boyne Resorts</li>
-							<li>Fifth Degree Tours</li>
-							<li>Gaylord Hotels</li>
-							<li>Marriott</li>
-							<li>Newcastle Casino</li>
-							<li>Riverwind Casino</li>
-							<li>Remington Park</li>
-							<li>Silverleaf Resorts</li>
-							<li>Tauck</li>
-							<li>Westport Country Playhouse</li>
-							<li>WinStar World Casino</li>
-						</ul>
-						<strong class="ttl">Home, Energy &amp; Retail</strong>
-						<ul class="list">
-							<li>Astrum Solar</li>
-							<li>Atlas Energy</li>
-							<li>Black and Decker</li>
-							<li>Blu Homes</li>
-							<li>Canine Fence Company</li>
-							<li>Cumberland Farms</li>
-							<li>Guess Watches</li>
-							<li>Gym Source</li>
-						</ul>
-				</div><!-- / accordion -->
+				<?php // <h2 id="clients">CLIENTS</h2> ?>
+				<?php the_content(); ?>
+								
+				<?php $page_title = 'Client List';  //Page ID
+				$page_data = get_page_by_title( $page_title ); ?>
+				<?php $content = apply_filters('the_content', $page_data->post_content);
+				//display the title and content
+				echo $content; ?>
 				<h2 id="case">CASE STUDIES</h2>
 				<p>Examples of how we help clients get more advertising results.</p>
 				<div class="gallery-holder">
 					<div class="slideset">
 						<ul class="slides">
-							<li data-thumb="<?php echo get_bloginfo('template_directory').'/'; ?>images/slide1-thumb.jpg">
-								<img src="<?php echo get_bloginfo('template_directory').'/'; ?>images/img-007.jpg" alt="image">
-							</li>
-							<li data-thumb="<?php echo get_bloginfo('template_directory').'/'; ?>images/slide2-thumb.jpg">
-								<img src="<?php echo get_bloginfo('template_directory').'/'; ?>images/img-008.jpg" alt="image">
-							</li>
-							<li data-thumb="<?php echo get_bloginfo('template_directory').'/'; ?>images/slide3-thumb.jpg">
-								<img src="<?php echo get_bloginfo('template_directory').'/'; ?>images/img-009.jpg" alt="image">
-							</li>
-							<li data-thumb="<?php echo get_bloginfo('template_directory').'/'; ?>images/slide4-thumb.jpg">
-								<img src="<?php echo get_bloginfo('template_directory').'/'; ?>images/img-007.jpg" alt="image">
-							</li>
-							<li data-thumb="<?php echo get_bloginfo('template_directory').'/'; ?>images/slide5-thumb.jpg">
-								<img src="<?php echo get_bloginfo('template_directory').'/'; ?>images/img-008.jpg" alt="image">
-							</li>
-							<li data-thumb="<?php echo get_bloginfo('template_directory').'/'; ?>images/slide6-thumb.jpg">
-								<img src="<?php echo get_bloginfo('template_directory').'/'; ?>images/img-009.jpg" alt="image">
-							</li>
+							<?php 
+							   // get the leadership fields
+							   if(get_field('gallery')){ ?>
+							 
+								<?php $counter = 0; ?>
+							 
+								<?php while(has_sub_field('gallery')){ ?>
+									<?php // get image id
+										$image_id = get_sub_field('slide_image');
+										//echo $image_id;
+										$thumb = wp_get_attachment_image_src( $image_id, 'case-thumb' );
+										$photo = wp_get_attachment_image( $image_id, 'case-image' );
+									    // echo $photo; 
+									     ?>
+									     <li data-thumb="<? echo $thumb[0]; ?>">
+									     	<?php echo $photo; ?>
+									     	<h2 class="ttl"><?php the_sub_field('title');?></h2>
+									     	<?php the_sub_field('content'); ?>
+									     </li>
+								<?php 
+									$counter++;
+									} // endwhile; ?>
+							<?php } //endif; ?>
 						</ul>
 					</div><!-- / slideset -->
 				</div><!-- / slideshow -->
-				<h2 class="ttl">Solar City <sup>&reg;</sup></h2>
-				<p>When Robert Downey Jr. was offered the role of Tony Stark in Iron Man, his first thought for learning the role was to speak to Elon Musk, the real-world rocket scientist behind Tesla, SpaceX and SolarCity. We were proud to work with Elon’s SolarCity for media planning and buying that helped the solar company triple its residential market share over two years. SolarCity became the No. 1 solar firm in the U.S. and achieved a successful IPO. The campaign worked so well that one of our vice presidents put solar panels on his home’s roof.</p>
+				
 			</section><!-- / content -->
 			<?php get_sidebar(); ?>
 		</section><!-- / main -->
